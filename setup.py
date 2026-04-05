5368cac1e4cef23ac45c23ecc6fd19f2dac69553
abhii104 <jollyrancher1409@gmail.com>
2026-04-05 19:43:08 +0530

    file updated

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mcqgenerator',
+    version='0.0.1',
+    author='abhishek kurade',
+    author_email='jollyrancher1409@gmail.com',
+    install_requires=[
+        "groq",
+        "langchain",
+        "streamlit",
+        "python-dotenv",
+        "PyPDF2"
+    ],
+    packages=find_packages()
+)
